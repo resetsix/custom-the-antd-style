b5e6a87df5cabc16e2444dbe0f71991f54a46805
一闪一闪亮晶晶 <460730259@qq.com>
2023-03-24 21:01:16 +0800

    fitst commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "antd-style";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 调用 ThemeProvider 方法 */}
+    <ThemeProvider
+      theme={{
+        token: {
+          colorPrimary: "#ffff00",
+        },
+      }}
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
